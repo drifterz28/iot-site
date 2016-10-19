6a34c4d4286fedd01374245decce12244ced6d98
Chris Whitney <cwhitney@goldstar.com>
2016-10-19 15:06:49 -0700

    Update route for kegbot

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.get('/conditions', function(req, res) {
 });
 
 app.get('/kegbot', function(req, res) {
-  kegBot(res);
+  kegBot(req, res);
 });
 
-app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost')
-console.log('Magic happens on port 3000');
+app.listen(env.NODE_PORT || 8888, env.NODE_IP || 'localhost')
+console.log('Magic happens on port 8888');
 exports = module.exports = app;
--- app/kegbot.js
+++ app/kegbot.js
@@ -1,14 +1,28 @@
-var MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
 
-module.exports = (res) => {
-  var connection_string = '';
-  if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
-  }
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({"hello": connection_string}));
+module.exports = (req, res) => {
+  const query = req.query;
+  fs.readFile('kegs.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    if(Object.keys(query).length !== 0) {
+      let json = JSON.parse(data);
+      if(['kegOne', 'kegTwo'].indexOf(query.keg) === -1) {
+        res.status(403).send('{"error": "invalid param"}');
+        return false;
+      }
+      if(query.name) {
+        json[query.keg].name = query.name;
+      }
+      if(query.maxValue) {
+        json[query.keg].maxValue = query.maxValue;
+      }
+      fs.writeFileSync('kegs.json', JSON.stringify(json), 'utf8');
+    }
+    // read values from saved file to confirm save
+    fs.readFile('kegs.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data);
+    });
+  });
 };
