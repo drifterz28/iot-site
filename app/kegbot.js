6d45efd6107b80703fcf0f5a85a3e153e7bdcdd2
Chris Whitney <cwhitney@goldstar.com>
2016-10-26 10:55:27 -0700

    add options to send values over to server

--- app/kegbot.js
+++ app/kegbot.js
@@ -6,9 +6,27 @@ module.exports = (req, res) => {
     if (err) throw err;
     if(Object.keys(query).length !== 0) {
       let json = JSON.parse(data);
-      if(['kegOne', 'kegTwo'].indexOf(query.keg) === -1) {
+      if(['kegOne', 'kegTwo'].indexOf(query.keg) === -1 || !query.json) {
         res.status(403).send('{"error": "invalid param"}');
         return false;
+      }
+      if(query.json) {
+        // need to get json data
+        /*
+        {
+          fan: 1,
+          temp: 52.34,
+          humidity: 51.8,
+          kegOne: 556.92,
+          kegTwo: 18.48
+          }
+        */
+        const queryJson = JSON.parse(query.json);
+        json.kegOne.value = queryJson.kegOne;
+        json.kegTwo.value = queryJson.kegTwo;
+        json.fan = queryJson.fan;
+        json.temp = queryJson.temp;
+        json.humidity = queryJson.humidity;
       }
       if(query.name) {
         json[query.keg].name = query.name;
