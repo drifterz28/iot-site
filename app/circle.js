582b1d670700902f6d25b4021a3aba5386450685
Chris Whitney <cwhitney@goldstar.com>
2017-02-16 15:24:30 -0800

    add date and days

--- app/circle.js
+++ app/circle.js
@@ -3,6 +3,10 @@ const _ = require('lodash');
 const requestUrl = 'https://circleci.com/api/v1.1/project/github/goldstar/goldstar?';
 const ciToken = 'circle-token=4d88be7da3423ea99111cc549fd402ef680ff6b2&limit=';
 const limit = 30;
+const d = new.Date();
+const day = d.getDay();
+const hours = d.getHours();
+
 module.exports = (res) => {
   request({url: requestUrl + ciToken + limit, json: true}, function(error, response, json) {
     if(!error) {
@@ -10,7 +14,7 @@ module.exports = (res) => {
         return build.user.login === 'drifterz28';
       });
       res.setHeader('Content-Type', 'application/json');
-      res.send(`{"status": "${buildStatus.status}"}`);
+      res.send(`{"status": "${buildStatus.status}", "dayOfTheWeek": "${day}", "hoursOfDay": "${hours}"}`);
     }
   });
 };
