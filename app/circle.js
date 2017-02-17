20d62acbe32069c42256aed5b4f9227ebb424c10
Chris Whitney <cwhitney@goldstar.com>
2017-02-17 11:16:43 -0800

    change the payload

--- app/circle.js
+++ app/circle.js
@@ -3,6 +3,28 @@ const _ = require('lodash');
 const requestUrl = 'https://circleci.com/api/v1.1/project/github/goldstar/goldstar?';
 const ciToken = 'circle-token=4d88be7da3423ea99111cc549fd402ef680ff6b2&limit=';
 const limit = 30;
+const badStatus = ['not_run', 'canceled'];
+const colorDefault = {
+  r: '000',
+  g: '000',
+  b: '000'
+}
+
+const colors = {
+  fixed: {g: '240'},
+  success: {g: '240'},
+  failed: {r: '240'},
+  running: {b: '240'},
+  scheduled: {
+    b: '211',
+    r: '148'
+  },
+  not_running: {
+    b: '211',
+    r: '148'
+  },
+  off: colorDefault
+};
 
 module.exports = (res) => {
   const d = new Date();
@@ -10,22 +32,28 @@ module.exports = (res) => {
   const hours = d.getHours();
   const isWeekEnd = day === 6 || day === 0;
   const isOffHours = hours < 10 || hours > 21;
-  
+
   request({url: requestUrl + ciToken + limit, json: true}, function(error, response, json) {
     if(!error) {
       let buildStatus = _.find(json, (build) => {
         return build.user.login === 'drifterz28';
       });
-      if(!buildStatus || buildStatus.status === 'canceled' || buildStatus.status === 'not_run') {
+      if(!buildStatus || badStatus.indexOf(buildStatus.status) > 0) {
         buildStatus = _.find(json, (build) => {
-          return build.status !== 'canceled' || build.status === 'not_run';
+          return badStatus.indexOf(build.status) === -1;
         });
       }
       if(isOffHours || isWeekEnd) {
-        //buildStatus.status = 'off';
+        buildStatus.status = 'off';
       }
+      const color = Object.assign(colorDefault, colors[buildStatus.status]);
+      let jsonOut = {
+        status: buildStatus.status,
+        time: d,
+      }
+      jsonOut = Object.assign(jsonOut, color);
       res.setHeader('Content-Type', 'application/json');
-      res.send(`{"status": "${buildStatus.status}", "time": "${d}"}`);
+      res.send(JSON.stringify(jsonOut));
     }
   });
 };
