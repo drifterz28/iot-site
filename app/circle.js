aa817949bd1a8014549289d8ceee66c8ffba6c20
Chris Whitney <cwhitney@goldstar.com>
2017-02-17 10:10:02 -0800

    update how status is used

--- app/circle.js
+++ app/circle.js
@@ -3,26 +3,29 @@ const _ = require('lodash');
 const requestUrl = 'https://circleci.com/api/v1.1/project/github/goldstar/goldstar?';
 const ciToken = 'circle-token=4d88be7da3423ea99111cc549fd402ef680ff6b2&limit=';
 const limit = 30;
-const d = new Date();
-const day = d.getDay();
-const hours = d.getHours();
-const isWeekEnd = day === 6 || day === 0;
-const isOffHours = hours < 10 || hours > 21;
 
 module.exports = (res) => {
+  const d = new Date();
+  const day = d.getDay();
+  const hours = d.getHours();
+  const isWeekEnd = day === 6 || day === 0;
+  const isOffHours = hours < 10 || hours > 21;
+  
   request({url: requestUrl + ciToken + limit, json: true}, function(error, response, json) {
     if(!error) {
       let buildStatus = _.find(json, (build) => {
         return build.user.login === 'drifterz28';
       });
       if(!buildStatus || buildStatus.status === 'canceled' || buildStatus.status === 'not_run') {
-        buildStatus = json[0];
+        buildStatus = _.find(json, (build) => {
+          return build.status !== 'canceled' || build.status === 'not_run';
+        });
       }
       if(isOffHours || isWeekEnd) {
         //buildStatus.status = 'off';
       }
       res.setHeader('Content-Type', 'application/json');
-      res.send(`{"status": "${buildStatus.status}"}`);
+      res.send(`{"status": "${buildStatus.status}", "time": "${d}"}`);
     }
   });
 };
