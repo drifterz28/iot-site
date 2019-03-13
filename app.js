cff31809f0d1a62f0e99f111c1feb615ec3b8049
Chris Whitney <cwhitney@goldstar.com>
2019-03-13 10:59:41 -0700

    update to use parcel

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 const env = process.env;
 const express = require('express');
+const Bundler = require('parcel-bundler');
 const app = express();
 
 const skiConditions = require('./app/ski-conditions');
-const kegBot = require('./app/kegbot');
-const circle = require('./app/circle');
 const pir = require('./app/pir');
 const sprinkler = require('./app/sprinkler');
 const date = require('./app/date');
 const crypto = require('./app/crypto');
 const test = require('./app/test');
 const goldstar = require('./app/goldstar');
-
-app.set('port', (process.env.PORT || 5000));
+const bundler = new Bundler('./src/index.html');
+const port = process.env.PORT || 5000;
+app.set('port', port);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static('static'));
+// app.use(express.static('dist'));
 
 app.get('/health', (req, res) => {
   res.send('good');
@@ -33,14 +33,6 @@ app.get('/goldstar', (req, res) => {
 
 app.get('/conditions', (req, res) => {
   skiConditions(res);
-});
-
-app.get('/kegbot', (req, res) => {
-  kegBot(req, res);
-});
-
-app.get('/circle', (req, res) => {
-  circle(res);
 });
 
 app.get('/pir', (req, res) => {
@@ -63,6 +55,8 @@ app.get('/test', (req, res) => {
   test(req, res);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.use(bundler.middleware());
+
+app.listen(port, () => {
+  console.log(`Node app is running on http://0.0.0.0:${port}`);
 });
